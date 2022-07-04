da04728852b107b1e3036ed8beeba77b5f3f62c1
tolyakryv <jp.komod.su@gmail.com>
2022-07-05 01:09:21 +0300

    changeModal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,38 +1,66 @@
 import propTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import s from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
-console.log(modalRoot);
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-  handleKeyDown = e => {
+
+const Modal = ({ onClose, showImg }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
+
+  const handleKeyDown = e => {
     if (e.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
-  overlayClick = e => {
+  const overlayClick = e => {
     if (e.currentTarget === e.target) {
-      this.props.onClose();
+      onClose();
     }
   };
-  render() {
-    return createPortal(
-      <div className={s.overlay} onClick={this.overlayClick}>
-        <div className={s.modal}>
-          <img src={this.props.showImg} alt="" />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <div className={s.overlay} onClick={overlayClick}>
+      <div className={s.modal}>
+        <img src={showImg} alt="" />
+      </div>
+    </div>,
+    modalRoot
+  );
+};
+
+// class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+//   handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+//   overlayClick = e => {
+//     if (e.currentTarget === e.target) {
+//       this.props.onClose();
+//     }
+//   };
+//   render() {
+//     return createPortal(
+//       <div className={s.overlay} onClick={this.overlayClick}>
+//         <div className={s.modal}>
+//           <img src={this.props.showImg} alt="" />
+//         </div>
+//       </div>,
+//       modalRoot
+//     );
+//   }
+// }
 export default Modal;
 Modal.propTypes = {
   onClose: propTypes.func.isRequired,
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,40 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import propTypes from 'prop-types';
 import s from './Searchbar.module.css';
-class Searchbar extends Component {
-  state = {
-    searchImg: '',
-  };
 
-  handleSubmit = e => {
+const Searchbar = ({ onSubmit }) => {
+  const [searchImg, setSearchImg] = useState('');
+  const handleSubmit = e => {
     e.preventDefault();
-
-    this.props.onSubmit(this.state.searchImg);
+    console.log(searchImg);
+    onSubmit(searchImg);
   };
-  changeInput = e => {
-    this.setState({ searchImg: e.currentTarget.value.trim() });
+  const changeInput = e => {
+    setSearchImg(e.currentTarget.value.trim());
   };
-  render() {
-    return (
-      <header className={s.searchbar}>
-        <form className={s.searchForm} onSubmit={this.handleSubmit}>
-          <button type="submit" className={s.searchFormButton}>
-            <span className={s.searchFormButtonLabel}>Search</span>
-          </button>
+  return (
+    <header className={s.searchbar}>
+      <form className={s.searchForm} onSubmit={handleSubmit}>
+        <button type="submit" className={s.searchFormButton}>
+          <span className={s.searchFormButtonLabel}>Search</span>
+        </button>
 
-          <input
-            className={s.searchFormInput}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            onChange={this.changeInput}
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className={s.searchFormInput}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          onChange={changeInput}
+        />
+      </form>
+    </header>
+  );
+};
+// class Searchbar extends Component {
+//   state = {
+//     searchImg: '',
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+
+//     this.props.onSubmit(this.state.searchImg);
+//   };
+//   changeInput = e => {
+//     this.setState({ searchImg: e.currentTarget.value.trim() });
+//   };
+//   render() {
+//     return (
+//       <header className={s.searchbar}>
+//         <form className={s.searchForm} onSubmit={this.handleSubmit}>
+//           <button type="submit" className={s.searchFormButton}>
+//             <span className={s.searchFormButtonLabel}>Search</span>
+//           </button>
+
+//           <input
+//             className={s.searchFormInput}
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             onChange={this.changeInput}
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
 export default Searchbar;
 Searchbar.propTypes = {
   onSubmit: propTypes.func.isRequired,
