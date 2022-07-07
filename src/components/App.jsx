a54698374d05a9f7f9f45142d1aa84ea09a09114
tolyakryv <jp.komod.su@gmail.com>
2022-07-07 17:12:21 +0300

    changeApp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,90 +1,151 @@
 import Searchbar from './Searchbar/Searchbar';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import imgAPI from '../services/imgAPI';
 import ImageGallery from './ImageGallery';
 import Button from './Button';
 import Loader from './Loader';
 import Modal from './Modal';
 import s from './App.module.css';
-class App extends Component {
-  state = {
-    images: [],
-    currentPage: 1,
-    searchText: '',
-    error: null,
-    total: null,
-    showModal: false,
-    largeImageURL: '',
-    showLoader: false,
-  };
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.searchText !== this.state.searchText ||
-      prevState.currentPage !== this.state.currentPage
-    ) {
-      this.fetchImg();
-      console.log(this.state);
+
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchText, setSearchText] = useState('');
+  const [error, setError] = useState(null);
+  const [total, setTotal] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [showLoader, setShowLoader] = useState(false);
+  useEffect(() => {
+    if (searchText !== '') {
+      fetchImg();
     }
-  }
-  onSubmitSearch = query => {
-    this.setState({
-      images: [],
-      currentPage: 1,
-      searchText: query,
-      error: null,
-      total: null,
-      showModal: false,
-      largeImageURL: '',
-      showLoader: false,
-    });
+  }, [searchText, currentPage]);
+
+  const onSubmitSearch = query => {
+    setImages([]);
+    setSearchText(query);
   };
-  fetchImg = () => {
-    this.setState({ showLoader: true });
-    const { searchText, currentPage } = this.state;
+  const fetchImg = () => {
+    setShowLoader(true);
+
     imgAPI(searchText, currentPage)
       .then(({ hits, totalHits }) => {
         const getImg = hits.map(({ id, webformatURL, largeImageURL }) => {
           return { id, webformatURL, largeImageURL };
         });
-        this.setState(prevState => ({
-          images: [...prevState.images, ...getImg],
-          total: totalHits,
-        }));
+        setImages([...images, ...getImg]);
+        setTotal(totalHits);
+        setShowLoader(false);
       })
-      .catch(error => this.setState({ error }))
-      .finally(() => this.setState({ showLoader: false }));
+      .catch(error => setError(error));
   };
-  onClickLoad = () => {
-    this.setState(prevState => ({ currentPage: prevState.currentPage + 1 }));
+  const onClickLoad = () => {
+    setCurrentPage(currentPage + 1);
   };
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
-  openModal = id => {
-    const findImg = this.state.images.find(e => e.id === id);
-    this.setState({ largeImageURL: findImg.largeImageURL });
-    this.toggleModal();
+  const openModal = id => {
+    const findImg = images.find(e => e.id === id);
+    setLargeImageURL(findImg.largeImageURL);
+    toggleModal();
   };
-  render() {
-    const { error, images, showLoader, showModal, largeImageURL, total } =
-      this.state;
-    return (
-      <div className={s.App}>
-        <Searchbar onSubmit={this.onSubmitSearch} />
-        {error && alert(error)}
-        {images.length > 0 && (
-          <ImageGallery openModal={this.openModal} images={images} />
-        )}
-        {showLoader && <Loader />}
-        {images.length > 0 && total !== images.length && (
-          <Button onClick={this.onClickLoad} />
-        )}
+  return (
+    <div className={s.App}>
+      <Searchbar onSubmit={onSubmitSearch} />
+      {error && alert(error)}
+      {images.length > 0 && (
+        <ImageGallery openModal={openModal} images={images} />
+      )}
+      {showLoader && <Loader />}
+      {images.length > 0 && total !== images.length && (
+        <Button onClick={onClickLoad} />
+      )}
 
-        {showModal && (
-          <Modal onClose={this.toggleModal} showImg={largeImageURL} />
-        )}
-      </div>
-    );
-  }
-}
+      {showModal && <Modal onClose={toggleModal} showImg={largeImageURL} />}
+    </div>
+  );
+};
 export default App;
+// class App extends Component {
+//   state = {
+//     images: [],
+//     currentPage: 1,
+//     searchText: '',
+//     error: null,
+//     total: null,
+//     showModal: false,
+//     largeImageURL: '',
+//     showLoader: false,
+//   };
+//   componentDidUpdate(prevProps, prevState) {
+//     if (
+//       prevState.searchText !== this.state.searchText ||
+//       prevState.currentPage !== this.state.currentPage
+//     ) {
+//       this.fetchImg();
+//       console.log(this.state);
+//     }
+//   }
+//   onSubmitSearch = query => {
+//     this.setState({
+//       images: [],
+//       currentPage: 1,
+//       searchText: query,
+//       error: null,
+//       total: null,
+//       showModal: false,
+//       largeImageURL: '',
+//       showLoader: false,
+//     });
+//   };
+//   fetchImg = () => {
+//     this.setState({ showLoader: true });
+//     const { searchText, currentPage } = this.state;
+//     imgAPI(searchText, currentPage)
+//       .then(({ hits, totalHits }) => {
+//         const getImg = hits.map(({ id, webformatURL, largeImageURL }) => {
+//           return { id, webformatURL, largeImageURL };
+//         });
+//         this.setState(prevState => ({
+//           images: [...prevState.images, ...getImg],
+//           total: totalHits,
+//         }));
+//       })
+//       .catch(error => this.setState({ error }))
+//       .finally(() => this.setState({ showLoader: false }));
+//   };
+//   onClickLoad = () => {
+//     this.setState(prevState => ({ currentPage: prevState.currentPage + 1 }));
+//   };
+//   toggleModal = () => {
+//     this.setState(({ showModal }) => ({ showModal: !showModal }));
+//   };
+//   openModal = id => {
+//     const findImg = this.state.images.find(e => e.id === id);
+//     this.setState({ largeImageURL: findImg.largeImageURL });
+//     this.toggleModal();
+//   };
+//   render() {
+//     const { error, images, showLoader, showModal, largeImageURL, total } =
+//       this.state;
+//     return (
+//       <div className={s.App}>
+//         <Searchbar onSubmit={this.onSubmitSearch} />
+//         {error && alert(error)}
+//         {images.length > 0 && (
+//           <ImageGallery openModal={this.openModal} images={images} />
+//         )}
+//         {showLoader && <Loader />}
+//         {images.length > 0 && total !== images.length && (
+//           <Button onClick={this.onClickLoad} />
+//         )}
+
+//         {showModal && (
+//           <Modal onClose={this.toggleModal} showImg={largeImageURL} />
+//         )}
+//       </div>
+//     );
+//   }
+// }
