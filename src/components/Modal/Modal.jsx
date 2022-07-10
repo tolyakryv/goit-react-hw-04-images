eb4c2aacae6ca0c0f88ccc1b62c55b7f63e9e4c9
tolyakryv <jp.komod.su@gmail.com>
2022-07-10 14:18:56 +0300

    changeModalEfect

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -7,17 +7,17 @@ const modalRoot = document.querySelector('#modal-root');
 
 const Modal = ({ onClose, showImg }) => {
   useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
     window.addEventListener('keydown', handleKeyDown);
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  });
+  }, [onClose]);
 
-  const handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      onClose();
-    }
-  };
   const overlayClick = e => {
     if (e.currentTarget === e.target) {
       onClose();
@@ -33,34 +33,6 @@ const Modal = ({ onClose, showImg }) => {
   );
 };
 
-// class Modal extends Component {
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.handleKeyDown);
-//   }
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.handleKeyDown);
-//   }
-//   handleKeyDown = e => {
-//     if (e.code === 'Escape') {
-//       this.props.onClose();
-//     }
-//   };
-//   overlayClick = e => {
-//     if (e.currentTarget === e.target) {
-//       this.props.onClose();
-//     }
-//   };
-//   render() {
-//     return createPortal(
-//       <div className={s.overlay} onClick={this.overlayClick}>
-//         <div className={s.modal}>
-//           <img src={this.props.showImg} alt="" />
-//         </div>
-//       </div>,
-//       modalRoot
-//     );
-//   }
-// }
 export default Modal;
 Modal.propTypes = {
   onClose: propTypes.func.isRequired,
